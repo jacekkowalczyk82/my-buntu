0dbba4705b6438275980e4c8afb721786674e977
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2019-07-04 13:35:30 +0200

    fixed xubuntu install function to use apt-get

--- config/functions.sh
+++ config/functions.sh
@@ -4,7 +4,7 @@ install_custom_packages() {
 for package_line in $(< /packages.list); do 
     package_name=$(echo $package_line | grep -v \# | xargs -n 1 ) 
     if [ ! -z $package_name ]; then 
-		apt -y install $package_name 
+		apt-get -y install $package_name 
     fi 
 done 
 }
